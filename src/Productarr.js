cc73ca303042b2c2a7713f7a048374028d7e4b25
baser.nishant@gmail.com <baser.nishant@gmail.com>
2022-03-17 14:35:02 +0530

    Updated index.html with a new line

--- src/Productarr.js
+++ src/Productarr.js
@@ -5,48 +5,31 @@ const ProductArr = [{
     description: "Refreshing Drink"
 }, {
     id: 102,
-    image: "https://cdn2.bigcommerce.com/n-ou1isn/ibk3tn/product_images/uploaded_images/stage2-apricot-peach-pumpkin-front-403x659.png",
+    image: "https://m.media-amazon.com/images/I/51trg6+djWL._AC_UY575_.jpg",
     price: 30,
     description: "Refreshing Drink"
 }
 
     , {
     id: 103,
-    image: "https://cdn2.bigcommerce.com/n-ou1isn/ibk3tn/product_images/uploaded_images/stage2-apricot-peach-pumpkin-front-403x659.png",
+    image: "https://m.media-amazon.com/images/I/614SGZ52HnL._AC_UY575_.jpg",
     price: 30,
     description: "Refreshing Drink"
 
 },
 {
     id: 104,
-    image: "https://cdn2.bigcommerce.com/n-ou1isn/ibk3tn/product_images/uploaded_images/stage2-apricot-peach-pumpkin-front-403x659.png",
+    image: "https://m.media-amazon.com/images/I/71nz0bq+KVS._AC_UX569_.jpg",
     price: 30,
     description: "Refreshing Drink"
 }, {
     id: 105,
-    image: "https://cdn2.bigcommerce.com/n-ou1isn/ibk3tn/product_images/uploaded_images/stage2-apricot-peach-pumpkin-front-403x659.png",
+    image: "https://m.media-amazon.com/images/I/61gNJtd-KsL._AC_SX679_.jpg",
     price: 30,
     description: "Refreshing Drink"
 }, {
     id: 106,
-    image: "https://cdn2.bigcommerce.com/n-ou1isn/ibk3tn/product_images/uploaded_images/stage2-apricot-peach-pumpkin-front-403x659.png",
-    price: 30,
-    description: "Refreshing Drink"
-}, {
-    id: 107,
-    image: "https://cdn2.bigcommerce.com/n-ou1isn/ibk3tn/product_images/uploaded_images/stage2-apricot-peach-pumpkin-front-403x659.png",
-    price: 30,
-    description: "Refreshing Drink"
-},
-{
-    id: 108,
-    image: "https://cdn2.bigcommerce.com/n-ou1isn/ibk3tn/product_images/uploaded_images/stage2-apricot-peach-pumpkin-front-403x659.png",
-    price: 30,
-    description: "Refreshing Drink"
-},
-{
-    id: 109,
-    image: "https://cdn2.bigcommerce.com/n-ou1isn/ibk3tn/product_images/uploaded_images/stage2-apricot-peach-pumpkin-front-403x659.png",
+    image: "https://m.media-amazon.com/images/I/81qGuRXSSWL._SX679_.jpg",
     price: 30,
     description: "Refreshing Drink"
 }
